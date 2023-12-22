28f639630e5474e2255345219d80e9d5994555af
umar <umar.dev500@gmail.com>
2023-12-22 17:12:40 +0700

    add check auth middleware

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.21.5
 
 require (
 	github.com/go-playground/validator/v10 v10.16.0
+	github.com/gofiber/contrib/jwt v1.0.8
 	github.com/gofiber/fiber/v2 v2.51.0
 	github.com/golang-jwt/jwt/v5 v5.2.0
+	github.com/google/uuid v1.4.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
@@ -17,13 +19,13 @@ require (
 )
 
 require (
+	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/andybalholm/brotli v1.0.6 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/google/uuid v1.4.0 // indirect
 	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
---
+++ middleware/check_auth.go
@@ -0,0 +1,122 @@
+package auth
+
+import (
+	"context"
+	"os"
+	"strings"
+	"time"
+
+	jwtware "github.com/gofiber/contrib/jwt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog/log"
+	"github.com/umardev500/ekost/constants"
+	"github.com/umardev500/ekost/domain/model/errs"
+	"github.com/umardev500/ekost/utils"
+)
+
+type checkAuthMiddleware struct {
+	storage fiber.Storage
+}
+
+// NewCheckAuth creates a new checkAuthMiddleware instance.
+//
+// It takes a fiber.Storage parameter and returns a pointer to a checkAuthMiddleware struct.
+func NewCheckAuth(s fiber.Storage) *checkAuthMiddleware {
+	return &checkAuthMiddleware{
+		storage: s,
+	}
+}
+
+// Start returns a fiber.Handler that is responsible for handling the authentication middleware.
+//
+// It creates a new JWT middleware with the provided configuration, including a success handler
+// and a signing key derived from the "SECRET" environment variable. The returned handler can
+// be used to authenticate requests.
+//
+// Return:
+//   - fiber.Handler: The authentication middleware handler.
+func (cam *checkAuthMiddleware) Start() fiber.Handler {
+	return jwtware.New(jwtware.Config{
+		SuccessHandler: cam.CheckAuthSuccess,
+		SigningKey: jwtware.SigningKey{
+			Key: []byte(os.Getenv("SECRET")),
+		},
+	})
+}
+
+// CheckAuthSuccess checks the authorization of the request and proceeds with the next handler if authorization is successful.
+//
+// Parameters:
+// - c: The fiber.Ctx object representing the current HTTP request context.
+//
+// Return:
+// - err: An error object, if any error occurs during the authorization check.
+func (cam *checkAuthMiddleware) CheckAuthSuccess(c *fiber.Ctx) (err error) {
+	bearer := c.Get("Authorization")
+	claims := utils.GetMapclaims(c)
+	username := claims["username"].(string)
+	id := claims["id"].(string)
+	ctx := context.WithValue(c.Context(), constants.CtxKeyTx, bearer)
+	err = cam.CheckAuthService(ctx, username)
+	if err != nil {
+		return utils.ErrorHandler(c, err, fiber.StatusInternalServerError)
+	}
+
+	c.Locals("username", username)
+	c.Locals("id", id)
+
+	return c.Next()
+}
+
+// CheckAuthService checks the authentication service for a given user.
+//
+// It takes a context.Context object and a string parameter representing the user.
+// It returns an error.
+func (cam *checkAuthMiddleware) CheckAuthService(ctx context.Context, u string) (err error) {
+	log.Debug().Msgf("Starting check auth for user=%s", u)
+
+	bearer := ctx.Value(constants.CtxKeyTx).(string)
+	currentToken := strings.Split(bearer, " ")[1]
+
+	token, err := cam.storage.Get(u)
+	if err != nil {
+		id := uuid.New()
+		err = errs.CustomError{
+			ID:         id,
+			Message:    fiber.ErrInternalServerError.Message,
+			TimeStamp:  time.Now().UTC(),
+			StatusCode: fiber.StatusInternalServerError,
+		}
+		log.Error().Msgf("Error getting token [%s]", id)
+		return
+	}
+
+	if string(token) != currentToken {
+		id := uuid.New()
+		err = errs.CustomError{
+			ID:         id,
+			Message:    fiber.ErrUnauthorized.Message,
+			TimeStamp:  time.Now().UTC(),
+			StatusCode: fiber.StatusUnauthorized,
+		}
+		log.Error().Msgf("Can not access resources, invalid token user=%s [%s]", u, id)
+		return
+	}
+
+	if token == nil {
+		id := uuid.New()
+		err = errs.CustomError{
+			ID:         id,
+			Message:    constants.ErrMsgTokenNotFound,
+			TimeStamp:  time.Now().UTC(),
+			StatusCode: fiber.StatusNotFound,
+		}
+		log.Error().Msgf("Token not found user=%s [%s]", u, id)
+		return
+	}
+
+	log.Debug().Msgf("Check auth success for user=%s", u)
+
+	return
+}
