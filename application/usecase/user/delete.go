28daf3694d80b4384b4df227d689902e12fe46ed
umar <umar.dev500@gmail.com>
2023-12-26 10:15:31 +0700

    delete message

--- application/usecase/user/delete.go
+++ application/usecase/user/delete.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"context"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog/log"
 	"github.com/umardev500/kost/domain/model"
 	"github.com/umardev500/kost/utils"
@@ -14,13 +15,16 @@ func (uuc *userUsecase) Delete(ctx context.Context, data model.ActionData) (err 
 		newErr := utils.NewError()
 		log.Error().Msgf("Error while deleting user: id=%s %v", newErr.ID, err)
 
-		return
+		return newErr
 	}
 
 	err = utils.ErrAffected(aff, err)
 	if err != nil {
 		newErr := utils.NewError()
+		newErr.StatusCode = fiber.StatusOK
+		newErr.Message = "Delete not affected"
 		log.Warn().Msgf("Error while deleting user: id=%s %v", newErr.ID, err)
+		return newErr
 	}
 
 	return
